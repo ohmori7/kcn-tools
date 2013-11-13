a9367528d1c5a3e85dc5494ca817feedde11db3d
Motoyuki OHMORI <ohmori@tottori-u.ac.jp>
2013-11-13 19:33:52 +0900

    define dummy macros for BSD dependent macros.

---
+++ traceroute/traceroute.h
@@ -0,0 +1,23 @@
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif /* HAVE_CONFIG_H */
+
+#ifndef __RCSID
+#define __RCSID(a)
+#endif /* ! __RCSID */
+
+#ifndef __COPYRIGHT
+#define __COPYRIGHT(a)
+#endif /* ! __COPYRIGHT */
+
+#ifdef __GNUC__
+#define __dead		__attribute__((__noreturn__))
+#else /* __GNUC__ */
+#define __dead
+#endif /* ! __GNUC__ */
+
+#ifdef __GNUC__
+#define __unused	__attribute__((__unused__))
+#else /* __GNUC__ */
+#define __unused
+#endif /* ! __GNUC__ */
